53a86ac5928984dab7e31f771aec8758685f1e0e
Jean-Sebastien Legare <js@sitelabs.ca>
2019-12-04 18:02:18 -0800

    [wgs all] set high number of cpus

--- wgs_all/run.sh
+++ wgs_all/run.sh
@@ -5,5 +5,5 @@ set -exo pipefail
 HERE=$(cd "$(dirname "$0")" && pwd)
 (
     cd "$HERE/.."
-    python -m variants wgs_all/samples.json --reference "ha412" "$@"
+    python -m variants wgs_all/samples.json --reference "ha412" --maxvcpus 4096 "$@"
 )
